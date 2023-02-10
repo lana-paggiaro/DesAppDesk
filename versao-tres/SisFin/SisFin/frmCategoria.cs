e4d0de61e508788508a34efd170e3993dbc8043f
Lua-da-tarde <luanna@paggiaro.com.br>
2023-02-09 22:26:07 -0300

    pasta 7

---
+++ versao-tres/SisFin/SisFin/frmCategoria.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SisFin
+{
+    public partial class frmCategoria : Form
+    {
+        private bool Insercao = false;
+        private bool Edicao = false;
+
+        private Categoria categoria = new Categoria();
+        private List<Categoria> lstCategoria = new List<Categoria>();
+        private BindingSource bsCategoria;
+        
+        public frmCategoria()
+        {
+            InitializeComponent();
+            lstCategoria = categoria.GeraCategorias();
+        }
+
+        private void salvarRegistro(object sender, EventArgs e)
+        {
+            if (Insercao)
+            {
+                var nome = txtNome.Text.Trim();
+                var descr = txtDescricao.Text.Trim();
+                var tipo = rdReceita.Checked ? 1 : 2;
+                var status = chkStatus.Checked ? 1 : 0;
+                categoria.AddToList(3, nome, descr, tipo, status);
+            }
+
+            if (Edicao)
+            {
+                Categoria ct = lstCategoria.Find(item => item.Nome == txtNome.Text.Trim());
+                if (ct != null)
+                {
+                    ct.Descricao = txtDescricao.Text.Trim();
+                    ct.Tipo = rdReceita.Checked ? 1 : 2;
+                    ct.Status = chkStatus.Checked ? 1 : 0;
+                }
+            }
+
+            carregaGridCategoria();
+
+            MessageBox.Show("Registro gravado com sucesso!", "Aviso do sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            btnNovo.Enabled = true;
+            btnNovo.Focus();
+            txtNome.Enabled = true;
+            grpCategoria.Enabled = false;
+            btnAlterar.Enabled = true;
+            btnCancelar.Visible = false;
+            btnSalvar.Visible = false;
+            btnExcluir.Visible = true;
+            dgCategoria.Enabled = true; //novo
+
+            Insercao = false;
+            Edicao = false;
+        }
+
+        private void limparCampos()
+        {
+            txtNome.Clear();
+            txtDescricao.Clear();
+            rdDespesa.Checked = false;
+            rdReceita.Checked = false;
+            chkStatus.Checked = false;
+        }
+        
+        private void resetCampos()
+        {
+            limparCampos();
+            grpCategoria.Enabled = false;
+            btnAlterar.Enabled = true;
+            btnNovo.Enabled = true;
+            btnCancelar.Visible = false;
+            btnSalvar.Visible = false;
+            btnExcluir.Visible = false;
+            Insercao = false;
+            Edicao = false;
+        }
+
+        private void fecharForm(object sender, FormClosingEventArgs e)
+        {
+            if(Edicao || Insercao)
+            {
+                e.Cancel = true;
+                MessageBox.Show("Salve ou cancele antes de fechar!", "Aviso do sistema!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void frmCategoria_Load(object sender, EventArgs e)
+        {
+            txtNome.Text = "Combustível";
+            txtDescricao.Text = "Consumo de combustível";
+            rdDespesa.Checked = true;
+            chkStatus.Checked = true;
+            btnSalvar.Visible = false;
+            btnCancelar.Visible = false;
+            txtNome.Enabled = false;
+            txtDescricao.Enabled = false;
+            rdReceita.Enabled = false;
+            rdDespesa.Enabled = false;
+            chkStatus.Enabled = false;
+
+            dgCategoria.ColumnCount = 5;
+            dgCategoria.AutoGenerateColumns = false;
+            dgCategoria.Columns[0].Width = 50;
+            dgCategoria.Columns[0].HeaderText = "ID";
+            dgCategoria.Columns[0].DataPropertyName = "Id";
+            dgCategoria.Columns[0].Visible = false;
+            dgCategoria.Columns[1].Width = 200;
+            dgCategoria.Columns[1].HeaderText = "NOME";
+            dgCategoria.Columns[1].DataPropertyName = "Nome";
+            dgCategoria.Columns[2].Width = 400;
+            dgCategoria.Columns[2].HeaderText = "DESCRIÇÃO";
+            dgCategoria.Columns[2].DataPropertyName = "Descricao";
+            dgCategoria.Columns[3].Width = 50;
+            dgCategoria.Columns[3].HeaderText = "TIPO";
+            dgCategoria.Columns[3].DataPropertyName = "Tipo";
+            dgCategoria.Columns[4].Width = 50;
+            dgCategoria.Columns[4].HeaderText = "STATUS";
+            dgCategoria.Columns[4].DataPropertyName = "Status";
+
+            dgCategoria.AllowUserToAddRows = false;
+            dgCategoria.AllowUserToDeleteRows = false;
+            dgCategoria.MultiSelect = false;
+            dgCategoria.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            carregaGridCategoria();
+        }
+
+        private void carregaGridCategoria()
+        {
+            bsCategoria = new BindingSource();
+            bsCategoria.DataSource = lstCategoria;
+            //dg.Categoria.Rows.Clear();
+            dgCategoria.DataSource = bsCategoria;
+            dgCategoria.Refresh();
+        }
+
+
+        private void grpCategoria_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtDescricao_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rdReceita_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rdDespesa_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chkStatus_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            grpCategoria.Enabled = true;
+            limparCampos();
+            txtNome.Focus();
+            btnAlterar.Enabled = false;
+            btnCancelar.Visible = true;
+            btnSalvar.Visible = true;
+            btnExcluir.Visible = false;
+            btnNovo.Enabled = false;
+            Insercao = true;
+            Edicao = false;
+            txtNome.Enabled = true;
+            txtDescricao.Enabled = true;
+            rdReceita.Enabled = true;
+            rdDespesa.Enabled = true;
+            chkStatus.Enabled = true;
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            grpCategoria.Enabled = true;
+            limparCampos();
+            txtNome.Focus();
+            btnAlterar.Enabled = false;
+            btnCancelar.Visible = true;
+            btnSalvar.Visible = true;
+            btnExcluir.Visible = false;
+            btnNovo.Enabled = false;
+            Insercao = false;
+            Edicao = true;
+            txtNome.Enabled = true;
+            txtDescricao.Enabled = true;
+            rdReceita.Enabled = true;
+            rdDespesa.Enabled = true;
+            chkStatus.Enabled = true;
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Deseja mesmo excluir?","Aviso do sistema", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                limparCampos();
+                MessageBox.Show("Registro excluído com sucesso", "Aviso do sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                btnNovo.Focus();
+            }
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Registro gravado com sucesso!", "Aviso do sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            limparCampos();
+            btnNovo.Enabled = true;
+            btnNovo.Focus();
+            grpCategoria.Enabled = false;
+            btnAlterar.Enabled = true;
+            btnCancelar.Visible = false;
+            btnSalvar.Visible = false;
+            btnExcluir.Visible = true;
+            Insercao = false;
+            Edicao = false;
+            txtNome.Enabled = false;
+            txtDescricao.Enabled = false;
+            rdReceita.Enabled = false;
+            rdDespesa.Enabled = false;
+            chkStatus.Enabled = false;
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            btnNovo.Focus();
+            btnNovo.Enabled = true;
+            btnAlterar.Enabled = true;
+            btnExcluir.Visible = true;
+            btnSalvar.Visible = false;
+            btnCancelar.Visible = false;
+            txtNome.Enabled = false;
+            txtDescricao.Enabled = false;
+            rdReceita.Enabled = false;
+            rdDespesa.Enabled = false;
+            chkStatus.Enabled = false;
+            Insercao = false;
+            Edicao = false;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
